696616b206d0bd7f334b4c5800f707a0cbc6b570
amandarenee1 <arw0219@gmail.com>
2017-12-28 23:29:14 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,8 +3,8 @@
 def display_board(board)
 
 board = [" ", " ", " ", " ", "  ", " ", " ", " ", " "]
-board = [" ", " ", " ", " ", " X ", " ", " ", " ", " "]
-board = [" O ", " ", " ", " ", " X ", " ", " ", " ", " "]
+display_board = [" ", " ", " ", " ", " X ", " ", " ", " ", " "]
+display_board = [" O ", " ", " ", " ", " X ", " ", " ", " ", " "]
 puts " #{board[0]} | #{board[1]} | #{board[2]} "
 puts "-----------"
 puts " #{board[3]} | #{board[5]} | #{board[5]} "
